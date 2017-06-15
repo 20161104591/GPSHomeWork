53d721e8749bdab9d6de83f189e489a9ac63b458
20161104591 <346680424@qq.com>
2017-06-15 14:52:59 +0800

    文件
    文件输入输出

--- GPSHomeWork/main.c
+++ GPSHomeWork/main.c
@@ -8,8 +8,39 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    FILE *fr;
+    FILE *fw;
+    fr=fopen("//Users//a20161104591//Desktop//GPSHomeWork//input.txt","w");
+    fw=fopen("//Users//a20161104591//Desktop//GPSHomeWork//output.txt","w");
+    int i,j,n,m,s;
+    int a[10];
+    printf("请输入10个数字:\n");
+    while(scanf("%d",&n)!=EOF)
+    {
+        for(s=1;s<=11;s++)
+        {
+            scanf("%d\n",&a[s]);
+            fprintf(fr,"%d",a[s]);
+        }
+    }
+    for(j=0;j<=n-1;j++)
+    {
+        for(i=0;i<=n-1;i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                m=a[i];
+                a[i]=a[i+1];
+                a[i+1]=m;
+            }
+        }
+    }
+    for(s=0;s<i;s++)
+    {
+        printf("%d",a[i]);
+        fprintf(fr,"%d",a[s]);
+    }
     return 0;
 }
